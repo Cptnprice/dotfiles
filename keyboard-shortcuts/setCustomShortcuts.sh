329e0741c6ec4f215cb59aa22997a4aee41158c3
Shalva Chkuaselidze <shalvachkuaselidze77@gmail.com>
2026-03-27 16:14:02 +0400

    feat: add browser shortcut

---
+++ keyboard-shortcuts/setCustomShortcuts.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']" &&
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Firefox' &&
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'firefox' &&
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>b'
